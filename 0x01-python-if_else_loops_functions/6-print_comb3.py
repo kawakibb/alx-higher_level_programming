a4330b887c9e2bc5d896c7b16d0edbc97a91ef9d
kawakibb <kawakibk7@gmail.com>
2023-07-11 20:36:38 +0000

    error don't use variables l, O, I

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 for k in range(0, 9):
-    for l in range(k + 1, 10):
-        if k == 8 and l == 9:
-            print("{:d}{:d}".format(k, l))
+    for a in range(k + 1, 10):
+        if k == 8 and a == 9:
+            print("{:d}{:d}".format(k, a))
         else:
-            print("{:d}{:d}".format(k, l), end=", ")
+            print("{:d}{:d}".format(k, a), end=", ")
