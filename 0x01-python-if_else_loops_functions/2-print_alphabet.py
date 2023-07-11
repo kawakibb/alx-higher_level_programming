17c8e6deded6231ea18d1ab480d7957cb3b23211
kawakibb <kawakibk7@gmail.com>
2023-07-11 18:02:55 +0000

    print alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 
-a = 122
-while a >= 97:
-    x = 0
-    if a % 2 != 0:
-        a = a - 32
-        x = 1
-    print("{:s}".format(chr(a)), end="")
-    if x == 1:
-        a = a + 32
-    a = a - 1
+for k in range(ord('a'), ord('z') + 1):
+	print("{:s}".format(chr(k)), end="")
