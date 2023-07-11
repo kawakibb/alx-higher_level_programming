66737b72cde2b4ebeebdffe0bec6f48b571b6899
kawakibb <kawakibk7@gmail.com>
2023-07-11 22:17:00 +0000

    upper error output

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 
 def uppercase(str):
     for x in range(len(str)):
-        k = ord(str[k])
+        k = ord(str[x])
         if k >= 97 and k <= 122:
             k = k - 32
         print("{}".format(chr(k)), end="")
