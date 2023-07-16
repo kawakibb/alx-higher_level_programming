7bd310e372877d789fe46117c4b1579eae1252af
kawakibb <kawakibk7@gmail.com>
2023-07-16 11:32:29 -0700

    error import

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,8 @@
-#include <Python.h>
-#include <object.h>
+
+
+/*#include <object.h>
 #include <listobject.h>
+**/
 
 /**
 * print_python_list_info - prints basic phyton list info
---
+++ 0x03-python-data_structures/Pyobj.h
@@ -0,0 +1,17 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s -  linked list
+ * @p: integer
+ * @print: next node ponter
+ *
+ * Description: linked list
+ *
+ */
+
+void print_python_list_info(PyObject *p)
+
+#endif /* LISTS_H */
