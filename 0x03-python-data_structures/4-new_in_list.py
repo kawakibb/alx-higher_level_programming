8f027cef5be1cc7a185f0490221dba1303368ec9
kawakibb <kawakibk7@gmail.com>
2023-07-16 08:59:19 -0700

    4. Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,6 @@
+def new_in_list(my_list, idx, element):
+    new_list = my_list[:]
+    if 0 <= idx < len(new_list):
+        new_list[idx] = element
+        return (new_list)
+    return (my_list)
