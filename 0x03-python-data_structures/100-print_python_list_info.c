bc0c7eb287e87543963ad7b9f27c8ff6630980f2
kawakibb <kawakibk7@gmail.com>
2023-07-16 11:06:34 -0700

    12. Switch

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,21 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+/**
+* print_python_list_info - prints basic phyton list info
+* @p: py object
+*
+**/
+
+void print_python_list_info(PyObject *p)
+{
+	long int size_list = PyList_Size(p);
+	int a;
+	PyListObject *object = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %li\n", size_list);
+	printf("[*] Allocated = %li\n", object->allocated);
+	for (a = 0; a < size_list; a++)
+		printf("Element %i: %s\n", a, Py_TYPE(object->ob_item[i])->tp_name);
+}
