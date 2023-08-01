fa6cc16706c54520e1e5f9c4f1adf81db365b0e5
kawakibb <kawakibk7@gmail.com>
2023-08-01 23:05:19 +0000

    0x06-python-classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,11 +7,13 @@ class Square:
     Attributes:
         __size (int): size of a side of the square
     """
+
     def __init__(self, size):
         """Initializes a square
         Args:
             size (int): size of a side of the square
         Returns: None
         """
+
         self.__size = size
 
