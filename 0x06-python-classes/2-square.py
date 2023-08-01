1947602433b7bda0fb3179a4132ebe5618433988
kawakibb <kawakibk7@gmail.com>
2023-08-01 22:10:30 +0000

    0x06-python-classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-class Square():
-    """class square.
-    Attributes:
-        __size: size of square.
-    """
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
     def __init__(self, size=0):
-        """Initialization, only allowed type int > 0."""
-        if type(size) != int:
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if (int(size) < 0):
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
