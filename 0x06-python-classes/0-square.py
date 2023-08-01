9b7bc53ec971b6a890a388e165e2c5718128b283
kawakibb <kawakibk7@gmail.com>
2023-08-01 21:19:13 +0000

    0x06-python-classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""
-test comment for init
-"""
-class Square:
-    "class sqare contains a sqare square."
-    def __init__(self, msg, code):
-        "initialize a sqare, and make it square"
-        return
 
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+    pass
