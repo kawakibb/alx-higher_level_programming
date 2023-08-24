f564f6a6b5377359f73bf7743e348fe086e6a441
kawakibb <kawakibk7@gmail.com>
2023-08-24 18:03:34 +0000

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/0-main.py
+++ 0x0C-python-almost_a_circle/0-main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """ 0-main """
 from models.base import Base
 
--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,23 +1,21 @@
- a base model class."""
+#!/usr/bin/python3
+"""Defines a base model class."""
 import json
 import csv
 import turtle
 
 
 class Base:
-    """Base model.
-
-    This Represents the "base" for all other classes in project 0x0C*.
-
-    Private Class Attributes:
-        __nb_object (int): Number of instantiated Bases.
+    """Represent the base model.
+    Represents the "base" for all other classes in project 0x0C*.
+    Attributes:
+        __nb_objects (int): The number of instantiated Bases.
     """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
         """Initialize a new Base.
-
         Args:
             id (int): The identity of the new Base.
         """
@@ -30,7 +28,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Return the JSON serialization of a list of dicts.
-
         Args:
             list_dictionaries (list): A list of dictionaries.
         """
@@ -41,7 +38,6 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """Write the JSON serialization of a list of objects to a file.
-
         Args:
             list_objs (list): A list of inherited Base instances.
         """
@@ -56,7 +52,6 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """Return the deserialization of a JSON string.
-
         Args:
             json_string (str): A JSON str representation of a list of dicts.
         Returns:
@@ -70,7 +65,6 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """Return a class instantied from a dictionary of attributes.
-
         Args:
             **dictionary (dict): Key/value pairs of attributes to initialize.
         """
@@ -85,9 +79,7 @@ class Base:
     @classmethod
     def load_from_file(cls):
         """Return a list of classes instantiated from a file of JSON strings.
-
         Reads from `<cls.__name__>.json`.
-
         Returns:
             If the file does not exist - an empty list.
             Otherwise - a list of instantiated classes.
@@ -103,7 +95,6 @@ class Base:
     @classmethod
     def save_to_file_csv(cls, list_objs):
         """Write the CSV serialization of a list of objects to a file.
-
         Args:
             list_objs (list): A list of inherited Base instances.
         """
@@ -123,9 +114,7 @@ class Base:
     @classmethod
     def load_from_file_csv(cls):
         """Return a list of classes instantiated from a CSV file.
-
         Reads from `<cls.__name__>.csv`.
-
         Returns:
             If the file does not exist - an empty list.
             Otherwise - a list of instantiated classes.
@@ -147,7 +136,6 @@ class Base:
     @staticmethod
     def draw(list_rectangles, list_squares):
         """Draw Rectangles and Squares using the turtle module.
-
         Args:
             list_rectangles (list): A list of Rectangle objects to draw.
             list_squares (list): A list of Square objects to draw.
