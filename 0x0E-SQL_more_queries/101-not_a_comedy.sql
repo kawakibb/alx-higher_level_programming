3aa6c7b6fd56e2c2c2b918c7e6dd38493eae9370
kawakibb <kawakibk7@gmail.com>
2023-09-20 17:16:00 -0700

    SQL_more_queries

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,12 @@
+-- this file lists all shows without listing the genre Comedy in the database
+-- Database is using to list (all) rows not linked to one row
+SELECT title
+FROM tv_shows
+WHERE title NOT IN
+(SELECT title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy')
+GROUP BY title
+ORDER BY title ASC;
