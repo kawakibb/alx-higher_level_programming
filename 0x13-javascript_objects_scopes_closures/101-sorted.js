709e8bf6ecb8811488deda0991ccbb507c936946
kawakibb <kawakibk7@gmail.com>
2023-10-17 21:08:37 +0000

    javascript_objects

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-
 const dict = require('./101-data').dict;
-
 const totalist = Object.entries(dict);
 const vals = Object.values(dict);
 const valsUniq = [...new Set(vals)];
 const newDict = {};
-
 for (const j in valsUniq) {
   const list = [];
   for (const k in totalist) {
