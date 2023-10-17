453a61e78180674dccc1920dcb5a746fd6b81a59
kawakibb <kawakibk7@gmail.com>
2023-10-17 21:07:26 +0000

    javascript_objects

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-//concatenate arguments
 const fs = require('fs');
-
 const fArg = fs.readFileSync(process.argv[2]).toString();
 const sArg = fs.readFileSync(process.argv[3]).toString();
 fs.writeFileSync(process.argv[4], fArg + sArg);
