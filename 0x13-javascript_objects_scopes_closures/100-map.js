fb4b15c1810362d70255b61c32b6b163b24be090
kawakibb <kawakibk7@gmail.com>
2023-10-17 20:50:42 +0000

    javascript_objects

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//use 100-data.js
 const list = require('./100-data.js').list;
 console.log(list);
+//print a list in data file
 console.log(list.map((item, index) => item * index));
