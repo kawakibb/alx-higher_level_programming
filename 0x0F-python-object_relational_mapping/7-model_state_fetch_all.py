0f4d3173d18d75623cf0a0b818ebd6b614db96ce
kawakibb <kawakibk7@gmail.com>
2023-10-19 21:12:38 +0000

    python_ORM

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Start link class to table in the database, create a session, and list states.
+"""
+# Import necessary modules
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Check if the script is being run as the main program
+if __name__ == "__main__":
+    # Create a connection to the MySQL database using the provided arguments
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    
+    # Create the database tables based on the SQLAlchemy models (if they don't already exist)
+    Base.metadata.create_all(engine)
+    
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    # Query and list all states, ordered by their IDs
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
