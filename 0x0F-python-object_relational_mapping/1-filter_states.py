a3e7490af9d7af323b6b720614b9630b300a497e
kawakibb <kawakibk7@gmail.com>
2023-10-19 20:48:34 +0000

    python_ORM

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" Lists all states from the database hbtn_0e_0_usa """
-
+""" Lists all states from the database hbtn_0e_0_usa that start with 'N' """
 # Import necessary modules
 import MySQLdb
 import sys
@@ -8,13 +7,16 @@ import sys
 # Check if the script is being run as the main program
 if __name__ == "__main__":
     # Establish a connection to the MySQL database using command-line arguments
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     
     # Create a cursor for executing SQL queries
     cur = db.cursor()
     
-    # Execute an SQL query to select all rows from the 'states' table
-    cur.execute("SELECT * FROM states")
+    # Execute an SQL query to select rows from the 'states' table where the name starts with 'N'
+    # The 'LIKE BINARY' ensures case-sensitive matching, and the results are ordered by 'id'
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
     
     # Fetch all the rows returned by the query
     rows = cur.fetchall()
