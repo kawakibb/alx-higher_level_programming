1596fd960536a63e2826991e449e62c32977b64b
kawakibb <kawakibk7@gmail.com>
2023-10-21 11:04:55 +0000

    python ORM

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,12 +6,13 @@ from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
 mymetadata = MetaData()
-
 Base = declarative_base(metadata=mymetadata)
+
 
 class State(Base):
     """
-    SQLAlchemy model for the 'states' table with id and name attributes."""
+    SQLAlchemy model for the 'states' table with id and name attributes.
+    """
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
