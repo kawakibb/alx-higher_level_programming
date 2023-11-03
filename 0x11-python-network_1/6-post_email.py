ee2dbd8823f5cfbebb0ea90cd54a1f8a33a5436d
kawakibb <kawakibk7@gmail.com>
2023-11-03 18:00:01 +0000

    python-network

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""The script that takes in a URL,sends a request
+- to the URL and displays the value.
+"""
+
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
